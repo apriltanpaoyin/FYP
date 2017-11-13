eef18bc3996121ea4fbedf36d8ff1d4fef047d70
apriltanpaoyin <d14124009@mydit.ie>
2017-11-13 20:30:31 +0000

    Image Facial Detection ONLY

--- FYP/face.py
+++ FYP/face.py
@@ -1,40 +1,39 @@
 import cv2
 import os
 import numpy as np
-from matplotlib import pyplot as plt
 
-subjects = ["", "Jack Ma", "Mark Zuckerberg"]
+subjects = ["", "Jack Ma", "Mark Zuckerberg", "Pao Yin"]
 
 def detect_face(img):
-	G = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	face_cascade = cv2.CascadeClassifier("/home/pi/opencv-3.2.0/data/lbpcascades/lbpcascade_frontalface.xml")
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	face_cascade = cv2.CascadeClassifier('/home/pi/opencv/opencv/data/lbpcascades/lbpcascade_frontalface.xml')
 	
-	faces = face_cascade.detectMultiScale(G, scaleFactor = 1.2, minNeighbors = 5)
+	faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5)
 	
 	if (len(faces) == 0):
 		return None, None
 	
 	(x, y, w, h) = faces[0]
-	return G[y:y+w, x:x+h], faces[0]
+	return gray[y:y+w, x:x+h], faces[0]
 
-def prepare_training_data(data_path):
-	dirs = os.listdir(data_path)
+def prepare_training_data(data_folder_path):
+	dirs = os.listdir(data_folder_path)
 	faces = []
 	labels = []
 	
 	for dir_name in dirs:
 		if not dir_name.startswith("s"):
-			continue
+			continue;
 		
 		label = int(dir_name.replace("s", ""))
-		subject_path = data_path + "/" + dir_name
-		subject_img_names = os.listdir(subject_path)
+		subject_dir_path = data_folder_path + "/" + dir_name
+		subject_img_names = os.listdir(subject_dir_path)
 		
 		for img_name in subject_img_names:
 			if img_name.startswith("."):
-				continue
+				continue;
 			
-			img_path = subject_path + "/" + img_name
+			img_path = subject_dir_path + "/" + img_name
 			img = cv2.imread(img_path)
 			
 			face, box = detect_face(img)
@@ -43,11 +42,11 @@ def prepare_training_data(data_path):
 				faces.append(face)
 				labels.append(label)
 				
-	cv2.destroyAllWindows()
 	return faces, labels
-	
+
+# Train on images
 faces, labels = prepare_training_data("training-data")
-face_recognizer = cv2.face.createLBPHFaceRecognizer()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 face_recognizer.train(faces, np.array(labels))
 
 def draw_box(img, box):
@@ -60,19 +59,24 @@ def draw_txt(img, txt, x, y):
 def predict(img):
 	face, box = detect_face(img)
 	
-	label = face_recognizer.predict(face)
+	label, confidence = face_recognizer.predict(face)
 	label_txt = subjects[label]
 	
 	draw_box(img, box)
 	draw_txt(img, label_txt, box[0], box[1]-5)
 	return img
-	
+
+# Predict based on image
 I = cv2.imread("test-data/jack1.jpg")
 I2 = cv2.imread("test-data/zuck1.jpg")
+I3 = cv2.imread("test-data/myface.jpg")
 
 pred1 = predict(I)
 pred2 = predict(I2)
+pred3 = predict(I3)
 
-cv2.imshow(subjects[1], pred1)
-cv2.imshow(subjects[2], pred2)
+#cv2.imshow(subjects[1], pred1)
+#cv2.imshow(subjects[2], pred2)
+cv2.imshow(subjects[3], pred3)
 key = cv2.waitKey(0)
+cv2.destroyAllWindows()
