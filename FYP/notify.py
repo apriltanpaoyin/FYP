a13a52ac6940a3297ece15b8286edab19a558355
apriltanpaoyin <d14124009@mydit.ie>
2018-03-31 20:42:28 +0100

    Added motion check, removed old face detection

--- FYP/notify.py
+++ FYP/notify.py
@@ -3,7 +3,7 @@ import g_drive
 
 def notify():
 	push_service = FCMNotification(api_key="AAAA4p4edZs:APA91bGYTAl82ZRTtzX9bOfzaklxd1-oRcV2aGrKMhiOvKlBO349PsGmGbPhI995uAU7LDj7uehTwv3-qAAq5jV4zLm0LHoWZ2mE512UlcPyCju-QTnNxTwuEQvrm_kJqfSdYpx872zW")
-	registration_id = "cgKHWckdbRI:APA91bFYw5K-BIsUMsuTWEZRkEjtNmIuDj23wiUHsioxiCq2GcCOzmVU68aXCRXIm3LZxgWP4yTFYOTY_jwLuL6BRFaT_UFmbPaaLLy8qOSOsoUmh8CrJP0YU4MTIFiS-sXi6D8NsDZs"
+	registration_id = "dBpX7FeBbZg:APA91bHlDiuBccvuV_PH_Ht_rrMRIk7HWbm9mN0o-75wZnJRtYcmUuYF6LSua5UFHkahhHbv6nZaReUMI6jLaMMixC7T8fKCy_ciryWWvlrBCrY1W8FKJiFzSFP1emtoH_Dls8B71mVa"
 	message_title = "Motion Detected"
 	message_body = "Image added"
 	result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
